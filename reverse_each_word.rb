d6dd65b162b43f1e603da99d41ace5537bf09ce3
Allan <kipropsallan@gmail.com>
2022-08-19 14:51:35 +0300

    Done

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,1 +1,38 @@
-# Write your code here
+require 'pry'
+
+# def reverse_each_word(sentence)
+#     words = sentence.split
+#     reversed_words = []
+
+#     words.each do |word|
+#         reversed_words << word.reverse
+#     end
+#     reversed_words.join(" ")
+#   end
+
+#   puts reverse_each_word("Hello there, how are you?")
+
+#   binding.pry
+#   0
+
+# def reverse_each_word(sentence)
+#     words = sentence.split
+  
+#     reversed_words = words.map do |word|
+#       word.reverse
+#     end
+  
+#     reversed_words.join(" ")
+#   end
+#   puts reverse_each_word("Hello there, how are you?")
+
+#     binding.pry
+#     0
+
+def reverse_each_word(sentence)
+    sentence.split.map(&:reverse).join(" ")
+  end
+    puts reverse_each_word("Hello there, how are you?")
+
+    binding.pry
+    0
